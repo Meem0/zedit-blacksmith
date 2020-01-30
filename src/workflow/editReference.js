edc0cb0a8582bae248c3e93f02945c4dc19c134c
Meem0 <themeem0@gmail.com>
2020-01-29 22:09:50 -0800

    Tentative editReference directive

---
+++ src/workflow/editReference.js
@@ -0,0 +1,25 @@
+ngapp.directive('editReference', function() {
+    let editReferenceLink = function(scope) {
+        scope.chooseExisting = {
+            label: 'Choose Existing Record'
+        };
+        scope.defineHere = {
+            label: 'Define New Record Here'
+        };
+        scope.editMethods = [
+            scope.chooseExisting,
+            scope.defineHere
+        ];
+    };
+
+    return {
+        link: editReferenceLink,
+        restrict: 'E',
+        scope: {
+            model: '=',
+            signature: '@'
+        },
+        templateUrl: `${modulePath}/partials/editReference.html`,
+        transclude: true
+    }
+});
