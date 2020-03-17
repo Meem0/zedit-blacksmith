b247890616b7f91c197e3188827a0d282c95d9d3
Meem0 <themeem0@gmail.com>
2020-03-17 16:46:12 -0700

    Remove gulp tasks for copying to external folder

--- gulpfile.js
+++ gulpfile.js
@@ -50,19 +50,5 @@ function release() {
         .pipe(gulp.dest('.'));
 }
 
-function cleanRemote() {
-    return del('../zedit/modules/blacksmith', { force: true });
-}
-
-function copyRemote() {
-    return gulp.src('dist/**/*', { base: 'dist/' })
-        .pipe(gulp.dest('../zedit/modules/blacksmith'));
-}
-
 exports.release = release;
 exports.build = gulp.series(clean, build);
-exports.default = gulp.series(
-    gulp.parallel(clean, cleanRemote),
-    build,
-    copyRemote
-);
