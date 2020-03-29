d66d3d3f158eb4424dfee58906e76eca138b51d4
Meem0 <themeem0@gmail.com>
2020-03-29 10:54:53 -0700

    Remove commented out code

--- src/views/setItemAttributes.js
+++ src/views/setItemAttributes.js
@@ -1,53 +1,4 @@
 ngapp.run(function(workflowService, skyrimAttributeService) {
-    const weaponAttributeDefinitions = {
-        /*equipType: {
-            keyPath: ['ETYP']
-        },
-        blockBashImpactDataSet: {
-            keyPath: ['BIDS']
-        },
-        alternateBlockMaterial: {
-            keyPath: ['BAMT']
-        },
-        keywords: {
-            keyPath: ['KWDA']
-        },
-        impactDataSet: {
-            keyPath: ['INAM']
-        },
-        nif1st: {
-            keyPath: ['WNAM']
-        },
-        attackFailSound: {
-            keyPath: ['TNAM']
-        },
-        equipSound: {
-            keyPath: ['NAM9']
-        },
-        unequipSound: {
-            keyPath: ['NAM8']
-        },*//*,
-        animationType: {
-            keyPath: ['DNAM', 'Animation Type']
-        },
-        speed: {
-            keyPath: ['DNAM', 'Speed']
-        },
-        reach: {
-            keyPath: ['DNAM', 'Reach']
-        },
-        stagger: {
-            keyPath: ['DNAM', 'Stagger']
-        }
-        percentMultiplier: {
-            keyPath: ['CRDT', '% Mult']
-        }*/
-    };
-
-    const gearAttributeDefinitionsMap = {
-        weapon: weaponAttributeDefinitions
-    };
-
     // get the value for attributeName from attributeValues, or fall back to the attribute definition's default value
     let getAttributeValue = function(gearCategory, attributeName, itemType, material, attributeValues) {
         const attributeValue = attributeValues ? attributeValues[attributeName] : undefined;
@@ -55,11 +6,6 @@ ngapp.run(function(workflowService, skyrimAttributeService) {
     };
 
     let setItemAttributesController = function($scope) {
-        debugger;
-
-        // const itemAttributeValues = $scope.model.itemAttributeValues;
-        // const attributeValues = itemAttributeValues[itemEditorId];
-        // const attributeValue = attributeValues[attributeName];
         if (!$scope.model.itemAttributeValues) {
             $scope.model.itemAttributeValues = {};
         }
