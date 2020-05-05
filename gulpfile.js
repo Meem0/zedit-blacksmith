52cbe4533d0dffa9c4f8d14d2f0231ec9b62ff88
Meem0 <themeem0@gmail.com>
2020-05-05 16:51:22 -0700

    Add gulp setup step for creating symlinks

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
+const jetpack = require('fs-jetpack');
+const readline = require('readline');
 const del = require('del');
 const gulp = require('gulp');
 const rename = require('gulp-rename');
@@ -53,5 +55,49 @@ function release() {
         .pipe(gulp.dest('.'));
 }
 
+function setup(cb) {
+    let validatePath = function(path, cb) {
+        if (!jetpack.exists(path)) {
+            cb(new Error('Could not find path ' + path));
+            return false;
+        }
+        return true;
+    }
+
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    
+    rl.question('Enter zedit dev folder (e.g. "C:/Projects/zedit", "../zedit"): ', function(zeditDir) {
+        rl.close();
+
+        let zeditModulesDir = jetpack.cwd(zeditDir, 'modules');
+        let zeditGeneralDir = jetpack.cwd(zeditDir, 'src\\stylesheets\\general');
+        let zeditThemesDir = jetpack.cwd(zeditDir, 'src\\stylesheets\\themes');
+    
+        if (validatePath(zeditModulesDir.cwd(), cb) && validatePath(zeditGeneralDir.cwd(), cb) && validatePath(zeditThemesDir.cwd(), cb)) {
+            let zeditBlacksmithModuleDir = zeditModulesDir.cwd('blacksmith');
+            let blacksmithDistDir = jetpack.cwd('dist');
+            let blacksmithLibDir = jetpack.cwd('stylesheets\\lib');
+            let blacksmithThemesDir = jetpack.cwd('stylesheets\\themes');
+
+            jetpack.remove(zeditBlacksmithModuleDir.cwd());
+            jetpack.remove(blacksmithLibDir.cwd());
+            jetpack.remove(blacksmithThemesDir.cwd());
+        
+            fs.symlinkSync(blacksmithDistDir.cwd(), zeditBlacksmithModuleDir.cwd(), 'dir');
+            console.log(`Created symlink ${zeditBlacksmithModuleDir.cwd()} -> ${blacksmithDistDir.cwd()}`);
+            fs.symlinkSync(zeditGeneralDir.cwd(), blacksmithLibDir.cwd(), 'dir');
+            console.log(`Created symlink ${blacksmithLibDir.cwd()} -> ${zeditGeneralDir.cwd()}`);
+            fs.symlinkSync(zeditThemesDir.cwd(), blacksmithThemesDir.cwd(), 'dir');
+            console.log(`Created symlink ${blacksmithThemesDir.cwd()} -> ${zeditThemesDir.cwd()}`);
+
+            cb();
+        }
+    });
+}
+
 exports.release = release;
 exports.build = gulp.series(clean, build);
+exports.setup = setup;
